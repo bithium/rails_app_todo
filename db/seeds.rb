7bdca4f82f4581f7a65ce157715443243f7a1647
Bithium S.A <info@bithium.com>
2019-01-15 14:04:40 +0000

    ADD: Task model to application.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 #
+
+return unless Task.count.zero?
+
+10.times do |n|
+  Task.create(title: "Task #{n}")
+end
